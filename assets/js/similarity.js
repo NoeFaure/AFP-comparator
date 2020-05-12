0fbad6ceda41a6391288135f5047480f7731c619
NoeFaure <noe.faure@isep.fr>
2020-05-12 19:22:06 +0200

    Fix levenstein

--- assets/js/similarity.js
+++ assets/js/similarity.js
@@ -1,56 +1,18 @@
-// ======= Levenstein Algorithm =======
-function levDist(s, t) {
-    var d = []; //2d matrix
-
-    // Step 1
-    var n = s.length;
-    var m = t.length;
-
-    if (n == 0) return m;
-    if (m == 0) return n;
-
-    //Create an array of arrays in javascript (a descending loop is quicker)
-    for (var i = n; i >= 0; i--) d[i] = [];
-
-    // Step 2
-    for (var i = n; i >= 0; i--) d[i][0] = i;
-    for (var j = m; j >= 0; j--) d[0][j] = j;
-
-    // Step 3
-    for (var i = 1; i <= n; i++) {
-        var s_i = s.charAt(i - 1);
-
-        // Step 4
-        for (var j = 1; j <= m; j++) {
-
-            //Check the jagged ld total so far
-            if (i == j && d[i][j] > 4) return n;
-
-            var t_j = t.charAt(j - 1);
-            var cost = (s_i == t_j) ? 0 : 1; // Step 5
-
-            //Calculate the minimum
-            var mi = d[i - 1][j] + 1;
-            var b = d[i][j - 1] + 1;
-            var c = d[i - 1][j - 1] + cost;
-
-            if (b < mi) mi = b;
-            if (c < mi) mi = c;
-
-            d[i][j] = mi; // Step 6
-
-            //Damerau transposition
-            if (i > 1 && j > 1 && s_i == t.charAt(j - 2) && s.charAt(i - 2) == t_j) {
-                d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
-            }
-        }
-    }
-
-    // Step 7
-    return d[n][m];
-}
-
 function pourcentageLevenstein(textA, textB) {
+	
+	// Addtionial treatments
+	textA = textA.replace(/[']+/g, ' ');
+	textB = textB.replace(/[']+/g, ' ');
+	
+	textA = textA.replace(/[.]+/g, '');
+	textB = textB.replace(/[.]+/g, '');
+	
+	textA = textA.replace(/[,]+/g, '');
+	textB = textB.replace(/[,]+/g, '');
+	
+	// Convert string in arrays
+	textA = textA.split(" ");
+	textB = textB.split(" ");
 	
 	var levDis = levDist(textA,textB);
 	var bigger = Math.max(textA.length, textB.length);
@@ -58,23 +20,6 @@ function pourcentageLevenstein(textA, textB) {
 	return (bigger - levDis)/bigger;
 }
 // ======= Jaccard Algorithm =======
-
-function intersectTwo(arr1, arr2) {
-	var obj = {}, matched = [], unmatched = [];
-	for (var i = 0, l = arr1.length; i < l; i++) {
-			obj[arr1[i]] = (obj[arr1[i]] || 0) + 1;
-	}
-	for (i = 0; i < arr2.length; i++) {
-			var val = arr2[i];
-			if (val in obj) {
-					matched.push(val);
-			} else {
-					unmatched.push(val);
-			}
-	}
-  
-	return matched;
-}
 
 function intersect(a, b){
 	var common = [];
@@ -109,9 +54,10 @@ function jaccard(a,b) {
 	a = a.split(" ");
 	b = b.split(" ");
 	
+	//console.log(intersect(a, b));
   var intersection = intersect(a, b).length;
   var union = a.length + b.length;
-  var jacardIndex = (intersection / union);
+	var jacardIndex = (intersection / union);
 	
 	/*
 	alert("a ("+ a.length +"): " + a);
